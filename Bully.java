52a378eaa758e1cb1a4551060adb6c9dcee937c6
Igor Sgorla Brehm <igor.sgorla.brehm@hp.com>
2019-11-05 13:27:09 -0300

    expandida logica do coordenador

--- Bully.java
+++ Bully.java
@@ -1,16 +1,3 @@
-
-/**
- *  O nodo com o maior ID é o coordenador inicial, e será responsável por manter o estado de
- *  acesso aos semáforos. Os outros nodos enviam mensagens ao coordenador sempre que
- *  desejarem entrar e sair de uma região crítica. Se o coordenador não confirmar (a confirmação de
- *  recebimento deve ser feita mesmo quando negada a entrada na região crítica), então o deve-se
- *  iniciar uma eleição, de acordo com o algoritmo do valentão. Quando um novo coordenador for
- *  selecionado, os nodos devem começar a enviar as solicitações de entrada e saída de região
- *  crítica para o mesmo. É importante que o mecanismo seja validado eliminando-se o
- *  coordenador em algum momento. O coordenador deve ser o primeiro processo a ser carregado
- *  no ambiente para que o mecanismo funcione.
- */
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -87,6 +74,16 @@ public class Bully {
     }
   }
 
+/**
+ *  Se o coordenador não confirmar (a confirmação de
+ *  recebimento deve ser feita mesmo quando negada a entrada na região crítica), então o deve-se
+ *  iniciar uma eleição, de acordo com o algoritmo do valentão. Quando um novo coordenador for
+ *  selecionado, os nodos devem começar a enviar as solicitações de entrada e saída de região
+ *  crítica para o mesmo. É importante que o mecanismo seja validado eliminando-se o
+ *  coordenador em algum momento. O coordenador deve ser o primeiro processo a ser carregado
+ *  no ambiente para que o mecanismo funcione.
+ */
+
   // Metodo que performa a logica de um membro nao coordenador
   public static void groupMember(){
     // primeiro de tudo precisamos pedir ao coord que mande um ACK para sabermos que ele ja existe
@@ -112,39 +109,81 @@ public class Bully {
         String requesterIp = array[2];
         int requesterPort = Integer.parseInt(array[3]);
         Stats newRequester = new Stats(requesterId,requesterIp,requesterPort);
-        // acho que segundo o algoritmo eu deveria mandar uma mensagem de ACK aqui para o requisitor
 
         if(request.equals("ENTER")){ // se o pedido for de entrada na area critica
-          if(entryQueue.size() > 0 || (locked == true)){ // e a fila nao esta vazia ou o arquivo trancado
+          if(entryQueue.size() > 0 || (locked == true)){ // e a fila nao esta vazia ou o arquivo esta trancado
             entryQueue.add(newRequester); // entra na fila
             // enviar mensagem avisando que ele entrou na fila
-            //String message = "";
-            //sendData = message.getBytes();
-            
-            //DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName(destination_ip), destination_port);
-            //clientSocket.send(sendPacket);
+            String message = "STATUS-ONQUEUE";
+            sendData = message.getBytes();
+              
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName(requesterIp), requesterPort);
+            clientSocket.send(sendPacket);
           }
           else{ // senao
             locked = true; // ganha o acesso a area critica
             lockOwner = newRequester;
             // enviar mensagem avisando que ele ganhou acesso
+            String message = "STATUS-HASACCESS";
+            sendData = message.getBytes();
+              
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName(requesterIp), requesterPort);
+            clientSocket.send(sendPacket);
           }
         }
         else if(request.equals("LEAVE")){ // se o pedido for de saida da area critica
           if(lockOwner.idNumber == newRequester.idNumber){ // e ele for o dono atual do acesso ao arquivo
-            locked = false; // libero o arquivo
-            lockOwner = null; // removo o dono do acesso ao arquivo
+            if(entryQueue.size() == 0){ // e a fila esta vazia
+              locked = false; // libero o arquivo
+              lockOwner = null; // removo o dono do acesso ao arquivo
+            }
+            else{ // e a fila nao esta vazia
+              lockOwner = entryQueue.get(0); // altero o dono do acesso ao arquivo
+              entryQueue.remove(0); // removo o novo dono da fila de espera
+              // envio mensagem avisando ao novo dono do arquivo que ele ganhou acesso
+              String message = "STATUS-HASACCESS";
+              sendData = message.getBytes();
+                
+              DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName(requesterIp), requesterPort);
+              clientSocket.send(sendPacket);
+            }
+            
             // envio mensagem avisando que liberei a tranca conforme ele pediu
+            String message = "STATUS-LOSTACCESS";
+            sendData = message.getBytes();
+              
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName(requesterIp), requesterPort);
+            clientSocket.send(sendPacket);
           }
-          else{
+          else{ // e ele nao for o dono atual do acesso ao arquivo
             // envio mensagem avisando que ele nao e o dono do acesso ao arquivo e algo esta errado
+            String message = "STATUS-ERRORONREQUEST";
+            sendData = message.getBytes();
+              
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName(requesterIp), requesterPort);
+            clientSocket.send(sendPacket);
           }
+        }
+        else if(request.equals("ACK")){ // se o pedido for de apenas um ACK do coord
+          String message = "ACK"; // envio um ACK para o requisitor
+          sendData = message.getBytes();
+            
+          DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName(requesterIp), requesterPort);
+          clientSocket.send(sendPacket);
+        }
+        else if(request.equals("ELECTION")){ // se o pedido for de uma nova eleicao
+          // TODO
+          // comparo nossos ids e envio uma resposta?
+        }
+        else{ // se eu receber uma mensagem que nao reconheço 
+          throw new Exception("Mensagem Invalida Recebida"); // lanco uma nova excessao
         }
       }
       
     }
     catch(Exception exception){
-      System.out.println("Excecao no coord: "+exception.getStackTrace());
+      System.out.println("Excecao no coord: "+exception.getMessage());
+      System.out.println(exception.getStackTrace());
     }
   }
 }
