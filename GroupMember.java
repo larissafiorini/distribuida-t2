e7ea6158fd4cfa1bc9f6f3eed7a06ab4bc365600
igorbrehm <igorbrehm@hotmail.com>
2019-11-10 15:56:48 -0300

    Group member pronto, primeira versao

--- GroupMember.java
+++ GroupMember.java
@@ -24,29 +24,49 @@ public class GroupMember{
             byte[] receiveData = new byte[1024];
             byte[] sendData = new byte[1024];
 
-            // primeiro de tudo precisamos pedir ao coord que mande um ACK para sabermos que ele ja existe
-            String message = "ACK-"+Bully.myStats.idNumber+"-"+Bully.myStats.ipAddress+"-"+Bully.myStats.portNumber;
-            sendData = message.getBytes();
-            Stats myCoord = Bully.neighbours.get(coordId); // meu primeiro coord é o cara com id maior
+            while(true){
 
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName(myCoord.ipAddress), myCoord.portNumber);
-            clientSocket.send(sendPacket);
-            // agora preciso receber a mensagem de retorno
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            serverSocket.receive(receivePacket);
-            
-            String sentence = new String(receivePacket.getData(), receivePacket.getOffset(), receivePacket.getLength());
-            if(sentence.equals("ACK")){ // coord esta vivo, posso prosseguir
-                // TODO
-                // inicializo um produtor ou consumidor aqui
-            }
-            else{ // coord esta morto? Espero algum tempo e peço de novo
-                // TODO
-                // enviar ACK de novo
+                // primeiro de tudo precisamos pedir ao coord que mande um ACK para sabermos que ele ja existe
+                String message = "ACK-"+Bully.myStats.idNumber+"-"+Bully.myStats.ipAddress+"-"+Bully.myStats.portNumber;
+                sendData = message.getBytes();
+                Stats myCoord = Bully.neighbours.get(coordId); // meu primeiro coord é o cara com id maior
+
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName(myCoord.ipAddress), myCoord.portNumber);
+                clientSocket.send(sendPacket);
+                // agora preciso receber a mensagem de retorno
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                serverSocket.receive(receivePacket);
+                
+                String sentence = new String(receivePacket.getData(), receivePacket.getOffset(), receivePacket.getLength());
+                String array[] = sentence.split("-");
+                String command = array[0];
+                String role = array[1];
+
+                if(command.equals("ACK")){ // coord esta vivo, posso prosseguir
+                    if(role.equals("CONSUMER")){ // serei consumidor
+                        Consumer.execute(coordId);
+                    }
+                    else{ // serei produtor
+                        Producer.execute(coordId);
+                    }
+                    break;
+                }
+                else{ // coord esta morto? Espero algum tempo e peço de novo
+                    long lastPing = System.currentTimeMillis();
+                    while(true){
+
+                        long now = System.currentTimeMillis();
+
+                        if(((now - lastPing)/1000) >= 10){
+                            break;
+                        }
+                    }
+                }
             }
         }
         catch(Exception exception){
-
+            System.out.println("Excecao no groupMember: "+exception.getMessage());
+            System.out.println(exception.getStackTrace());
         }
     }
 }
