a1d90078a23532751ba4d4f03b55d5f1e849ab42
igorbrehm <igorbrehm@hotmail.com>
2019-11-17 22:11:09 -0300

    simplificacao do coordenador

--- GroupCoord.java
+++ GroupCoord.java
@@ -1,7 +1,6 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 
 /**
@@ -32,7 +31,6 @@ public class GroupCoord{
             
             if(socketType == 1){
                 serverSocket = new DatagramSocket(Bully.myStats.portNumber,InetAddress.getByName(Bully.myStats.ipAddress));
-                serverSocket.setSoTimeout(10000);
             }
             else{
                 clientSocket = new DatagramSocket();
@@ -71,37 +69,26 @@ public class GroupCoord{
                 Stats newRequester = new Stats(requesterId,requesterIp,requesterPort);
 
                 if(request.equals("PVAZIO")){
-                    System.out.println("PVAZIO");
                     P(1,newRequester);
                 }
 
                 else if(request.equals("VVAZIO")){
-                    System.out.println("VVAZIO");
-
                     V(1,newRequester);
                 }
 
                 else if(request.equals("PCHEIO")){
-                    System.out.println("PCHEIO");
-
                     P(2,newRequester);
                 }
 
                 else if(request.equals("VCHEIO")){
-                    System.out.println("VCHEIO");
-
                     V(2,newRequester);
                 }
 
                 else if(request.equals("PMUTEX")){
-                    System.out.println("PMUTEX");
-
                     P(3,newRequester);
                 }
 
                 else if(request.equals("VMUTEX")){
-                    System.out.println("VMUTEX");
-
                     V(3,newRequester);
                 }
 
@@ -112,7 +99,6 @@ public class GroupCoord{
 
                 else if(request.equals("CONSUME")){
                     System.out.println("Pedido para consumir");
-
                     currentBuffer = currentBuffer.substring(1);
                 }
 
@@ -121,13 +107,13 @@ public class GroupCoord{
                     System.out.println("Novo membro detectado.");
 
                     if(numProducers > numConsumers){
-                        System.out.println("Funcao atribuida CONSUMER.");
+                        System.out.println("Funcao atribuida: CONSUMER.");
 
                         message += "-CONSUMER";
                         numConsumers+=1;
                     }
                     else{
-                        System.out.println("Funcao atribuida PRODUCER.");
+                        System.out.println("Funcao atribuida: PRODUCER.");
 
                         message += "-PRODUCER";
                         numProducers+=1;
@@ -138,40 +124,10 @@ public class GroupCoord{
                     clientSocket.send(sendPacket);
                 }
 
-                else if(request.equals("ELECTION")){ // se o pedido for de uma nova eleicao
-                    
-                    // comparo nossos ids e envio uma resposta
-                    String message = "STATUS-"+Bully.myStats.idNumber+"-"+Bully.myStats.ipAddress+"-"+Bully.myStats.portNumber;
-                    sendData = message.getBytes();
-                        
-                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName(requesterIp), requesterPort);
-                    clientSocket.send(sendPacket);
-
-                    if(requesterId < Bully.myStats.idNumber){
-                        /**
-                        • Quando processo recebe msg de eleição de membros com ID mais baixa
-                        • Envia OK para remetente para indicar que está vivo e convoca eleição
-                        */
-                        callElection();
-                    }
-                }
-
-                else if(request.equals("NEWCOORD")){ // novo coordenador na area
-                    serverSocket.close();
-                    clientSocket.close();
-                    GroupMember.execute(requesterId);
-                    return;
-                }
-
                 else{ // se eu receber uma mensagem que nao reconheço 
                     throw new Exception("Mensagem Invalida Recebida: "+request); // lanco uma nova excessao
                 }
             }
-        }
-        catch(SocketTimeoutException e){
-            serverSocket.close();
-            clientSocket.close();
-            execute();
         }
         catch(Exception exception){
             System.out.println("Excecao no coord: "+exception.toString());
@@ -185,7 +141,9 @@ public class GroupCoord{
         try{
             switch(numSemaforo){
                 case 1: // PVAZIO
-        
+
+                    System.out.println("PVAZIO");
+
                     if(vazio == 0){
                         filaVazio.add(requester);
                         // enviar mensagem avisando que ele nao ganhou acesso
@@ -207,6 +165,9 @@ public class GroupCoord{
                     
                     break;
                 case 2: // PCHEIO
+
+                    System.out.println("PCHEIO");
+
                     if(cheio == 0){
                         filaCheio.add(requester);
                         // enviar mensagem avisando que ele nao ganhou acesso
@@ -227,6 +188,9 @@ public class GroupCoord{
                     }
                     break;
                 case 3: // PMUTEX
+
+                    System.out.println("PMUTEX");
+
                     if(mutex == 0){
                         filaMutex.add(requester);
                         // enviar mensagem avisando que ele nao ganhou acesso
@@ -262,6 +226,9 @@ public class GroupCoord{
         try{ 
             switch(numSemaforo){
                 case 1: // VVAZIO
+
+                    System.out.println("VVAZIO");
+
                     if(filaVazio.size() > 0){ // fila nao vazia
                         Stats vazioOwner = filaVazio.get(0);
                         // envio mensagem avisando o novo dono do mutex
@@ -284,6 +251,8 @@ public class GroupCoord{
                     break;
                 case 2: //VCHEIO
 
+                    System.out.println("VCHEIO");
+
                     if(filaCheio.size() > 0){ // fila nao vazia
                         Stats cheioOwner = filaCheio.get(0);
                         // envio mensagem avisando o novo dono do mutex
@@ -305,6 +274,9 @@ public class GroupCoord{
                     clientSocket.send(sendPacket);
                     break;
                 case 3: //VMUTEX
+
+                    System.out.println("VMUTEX");
+
                     if(filaMutex.size() > 0){ // fila nao vazia
                         Stats mutexOwner = filaMutex.get(0);
                         // envio mensagem avisando o novo dono do mutex
@@ -328,44 +300,6 @@ public class GroupCoord{
                 default:
                     break;
             }
-        }
-        catch(Exception exception){
-            System.out.println("Excecao no coord: "+exception.toString());
-            exception.printStackTrace();
-        }
-    }
-
-    // Metodo que performa uma eleicao
-    public static void callElection(){
-        try{
-            for(int i = 0; i < Bully.neighbours.size(); i++ ){
-                try{
-                    if(Bully.neighbours.get(i).idNumber > Bully.myStats.idNumber){ //envia msg de eleição para todos os processos com IDs maiores que o dele
-                        
-                        String message = "ELECTION-"+Bully.myStats.idNumber+"-"+Bully.myStats.ipAddress+"-"+Bully.myStats.portNumber;
-                        sendData = message.getBytes();
-                        
-                        Stats candidate = Bully.neighbours.get(i);
-                        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName(candidate.ipAddress), candidate.portNumber);
-                        clientSocket.send(sendPacket);
-                        // agora preciso receber a mensagem de retorno
-                        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                        serverSocket.receive(receivePacket);
-                        
-                        String sentence = new String(receivePacket.getData(), receivePacket.getOffset(), receivePacket.getLength());
-                        String array[] = sentence.split("-");
-
-                        int idNumber = Integer.parseInt(array[1]);
-                        if(idNumber > Bully.myStats.idNumber){ // Se algum processo com ID maior responde, ele desiste
-                            return;
-                        }
-                    }
-                }
-                catch(SocketTimeoutException e){
-                    continue;
-                }
-            }
-            //Se ninguém responde, P vence eleição e continua coordenador
         }
         catch(Exception exception){
             System.out.println("Excecao no coord: "+exception.toString());
